3e2c07572517ee7ca470ed0dc4e0fd3966463206
Bianca Barria <bianca.barria@beetrack.com>
2021-04-27 17:31:17 -0400

    feat: adding the Route to our App component

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
 class App extends React.Component {
     render () {
         return (
-            <div>Hello World!</div>
+            <div>
+                <Switch>
+                    <Route exac path="/" component={Home} />
+                    <Route exac path="/about" component={About} />
+                    <Route exac path="/contact" component={Contact} />
+                </Switch>
+            </div>
         )
     }
 }
