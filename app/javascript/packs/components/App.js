bb71ec0c0cb7795e7843c8c526d4da3046f48716
Bianca Barria <bianca.barria@beetrack.com>
2021-04-27 17:35:24 -0400

    feat: importing components to our App

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 class App extends React.Component {
     render () {
