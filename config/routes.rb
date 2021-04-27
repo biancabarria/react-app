96426b545801d7e7b8b17a6fc71314fcb308b786
Bianca Barria <bianca.barria@beetrack.com>
2021-04-27 17:37:03 -0400

    fix: typo and adding correct route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path', to: 'pages#index', via :all
 end
